ff230b19607613ee8b9cfa06afe5a14829049b6e
0x01 <143021151+VedantKhapre@users.noreply.github.com>
2026-04-28 18:45:56 +0530

    vercel-deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,12 @@ import { defineConfig, fontProviders } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import clerk from "@clerk/astro";
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), clerk()],
-  adapter: node({ mode: "standalone" }),
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
